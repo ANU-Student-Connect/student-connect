2b2961cfe39c6dd98562709f9493402e08a39ed4
U7112358 <u7112358@anu.edu.au>
2024-08-28 01:32:30 +1000

    added auth.js to routes and intiated the work

---
+++ server/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express.Router();
+Router.post('/signin' , function (req, res, next) {
+    const {email, password} = req.params;
+})
+
+module.exports = router;
