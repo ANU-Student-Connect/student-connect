473cdae58b95c52ca4776f05c59f4df00701e7ea
U7112358 <u7112358@anu.edu.au>
2024-08-28 02:27:35 +1000

    POST route in auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const app = express.Router();
-Router.post('/signin' , function (req, res, next) {
-    const {email, password} = req.params;
-})
+const router = express.Router();
+
+router.post('/signin', (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (email && password) {
+        res.cookie('user', email, { httpOnly: true });
+        return res.status(200).json({ message: 'Sign in successful', redirectUrl: '/home' });
+    } else {
+        return res.status(400).json({ message: 'Email and password required' });
+    }
+});
 
 module.exports = router;
