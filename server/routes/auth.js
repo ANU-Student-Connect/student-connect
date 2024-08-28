80450f4adfd1c1aa6a987175b0915beef6a6d984
U7112358 <u7112358@anu.edu.au>
2024-08-28 16:48:01 +1000

    Implemented User Sign in using the fake data #31

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+
+// user signin POST route for fake login data
 router.post('/signin', (req, res, next) => {
     const { email, password } = req.body;
 
+    // there is no specific logic here as user should login using any email address and password
     if (email && password) {
+
+        // setting cookie to rememebr user's login status
         res.cookie('user', email, { httpOnly: true });
         return res.status(200).json({ message: 'Sign in successful', redirectUrl: '/home' });
     } else {
