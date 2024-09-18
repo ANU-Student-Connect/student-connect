ee5137a3d3a2e72a845c2f96eeb28ed95ca259a5
clevetong <clevetong@outlook.com>
2024-09-18 19:29:02 +1000

    refactor frontend code to use Vite

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Sidebar from '../../components/sidebar/sidebar';
+import MessageCenter from '../../components/messages/MessageContainer';
+
+const Home = () => { 
+    return (
+        <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <Sidebar />
+            <MessageCenter />
+        </div>
+    );
+};
+
+export default Home;
