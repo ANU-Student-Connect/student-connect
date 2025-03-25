ba76df30e13292abb50fe7fcfa57bf5af4564744
Junqiao Weng <116329733+nicelyhorse@users.noreply.github.com>
2025-03-26 01:13:51 +1100

    Message api error fixing

--- client/src/components/messages/Sidebar.jsx
+++ client/src/components/messages/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Search } from 'lucide-react';
-import FriendCard from './FriendCard';x
+import FriendCard from './FriendCard';
 
 const Sidebar = ({ friends, onFriendSelect }) => {
     return (
