049766e02b696e6162c37949fcec19d71de9890e
Junqiao Weng <116329733+nicelyhorse@users.noreply.github.com>
2025-04-12 01:45:54 +1000

    send message simplified

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -3,20 +3,14 @@ import Message from "../models/message.model.js";
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 
-export const sendMessage = async (req, res) => { 
+export const sendMessage = async (req, res) => {
     try { 
         const { message } = req.body;
         const { id: receiverId } = req.params;
         const senderId = req.user._id;
 
-        let conversation = await Conversation.findOne({
-            participants: { $all: [senderId, receiverId] },
-        })
-
-        if (!conversation) { 
-            conversation = await Conversation.create({
-                participants: [senderId, receiverId],
-            });
+        if (!message || message.trim() === "") {
+            return res.status(400).json({ message: "Message text cannot be empty" });
         }
 
         const newMessage = new Message({
@@ -25,14 +19,9 @@ export const sendMessage = async (req, res) => {
             message,
         });
 
-        if (newMessage) { 
-            conversation.messages.push(newMessage._id);
-        }
+        await newMessage.save();
 
         // SOCKET IO FUNCTIONALITY
-
-        // parallel running
-        await Promise.all([conversation.save(), newMessage.save()]);
 
         res.status(201).json(newMessage);
     } catch (error) { 
